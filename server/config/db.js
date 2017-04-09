de0ff3f629042500dc6f7381d3d59ce3e1af0801
aaujla55 <amandeep99555@gmail.com>
2017-04-09 15:26:09 -0400

    minor update in indicator css

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   //local MongoDB deployment ->
-  // "URI": "mongodb://localhost/stockapp"
+   //"URI": "mongodb://localhost/stockapp"
   //Remote MongoDB deployment -> let 
   "URI" : " mongodb://aaujla55:stockapp123@ds061288.mlab.com:61288/stockapp"
  
